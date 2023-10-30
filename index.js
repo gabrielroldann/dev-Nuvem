e2730fc76fb8e6092c377042757e77b471dfa28d
Gabriel Roldan <90779512+gabrielroldann@users.noreply.github.com>
2023-10-30 20:07:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 
+app.get('/', function(req, res) {
+   res.send("Página inicial");
+)};
+
+app.get('/contato', function(req, res) {
+   res.send("Página contato");
+)};
+
+app.get('/users', function(req, res) {
+   res.send("Página users");
+)};
+
 app.get('/listUsers', function (req, res) {
    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
       console.log( data );
@@ -19,7 +31,7 @@ app.get('/listUser/:id', function (req, res) {
     });
  })
 
-var server = app.listen(8081, function () {
+var server = app.listen(3000, function () {
    var host = server.address().address
    var port = server.address().port
    console.log("Example app listening at http://%s:%s", host, port)
